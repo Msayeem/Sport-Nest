b2ea27da6a4aff722c0afc89202cecc87451c033
Msayeem <msayeem223@gmail.com>
2026-05-17 22:44:58 +0600

    Featured section added

--- src/Components/Featured.jsx
+++ src/Components/Featured.jsx
@@ -1,9 +1,22 @@
+import Image from 'next/image';
 import React from 'react';
 
-const Featured = () => {
+const Featured = async() => {
+
+const res=await fetch('http://localhost:5000/facilities');
+const datas=await res.json();
+
     return (
         <div>
-            
+            <h1>Featured Facilities</h1>
+
+            {
+                datas.map(data=>
+                    <div key={data._id}>
+                        <Image src={data.imageUrl} width={200} height={100} alt={data.facilityName}></Image>
+                        </div>
+                )
+            }
         </div>
     );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Banner from "@/Components/Banner";
+import Featured from "@/Components/Featured";
 import Image from "next/image";
 
 export default function Home() {
   return (
    <div>
     <Banner></Banner>
+    <Featured></Featured>
    </div>
   );
 }
