b4e73318dd9f46963bcf8fdaa5b77a3820cf6e3f
Msayeem <msayeem223@gmail.com>
2026-05-17 21:36:04 +0600

    Banner added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,13 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Nav from "@/Components/Nav";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const montserrat = Montserrat({
+
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+
 
 export const metadata = {
   title: "Create Next App",
@@ -21,7 +18,7 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${montserrat.className} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
         <Nav></Nav>
