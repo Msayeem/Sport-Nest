603d177b7a548cf2430b8381f942b8f437ccdac1
Msayeem <msayeem223@gmail.com>
2026-05-22 22:01:13 +0600

    Search issue fixed

--- src/app/manage-facility/page.jsx
+++ src/app/manage-facility/page.jsx
@@ -1,7 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import {Button, Card, CloseButton} from "@heroui/react";
-
 import React from 'react';
 
 import DeleteBtn from '@/Components/DeleteBtn';
